92610ac219c2c92b72c5266f6d4ce04d5ec581d7
Marthe Bjornsen <69841912+Marthebjornsen98@users.noreply.github.com>
2021-09-28 12:56:53 +0200

    doing stuff

--- js/index.js
+++ js/index.js
@@ -1,21 +1,31 @@
 import listToArray from "./libs/listToArray.js";
 import { validateInputLength } from "./libs/validateInputLength.js";
 
-const searchBar = document.querySelector(".to-do__add");
-const button = document.querySelector(".to-do__btn");
-const items = document.querySelectorAll(".to-do__list");
-
 let listItems = [];
 
-button.onclick = function () {
-  if (validateInputLength(searchBar.value, 3)) {
-    items.innerHTML = "";
-    listItems.push(searchBar.value);
+const searchBar = document.querySelector(".to-do__add");
+const searchButton = document.querySelector(".to-do__btn");
+const items = document.querySelector(".to-do__list");
 
+searchButton.onclick = function () {
+  if (!validateInputLength(searchBar.value, 3)) {
+    console.log("Fail");
+
+    return;
+  } else {
+    items.innerHTML = "";
+
+    listItems.push(searchBar.value);
     listToArray(listItems, items);
 
+    let toggleList = document.querySelectorAll(".to-do__item");
+
+    toggleList.forEach((element) => {
+      element.onclick = () => {
+        element.classList.toggle("toggle");
+      };
+    });
+
     console.log("Pass");
-  } else {
-    console.log("Fail");
   }
 };
